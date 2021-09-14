29cf9981ee44f5177a32674d9f8820246c94d4da
justinnnlo <justinkevinhl@gmail.com>
2021-09-13 19:19:12 -0700

    push to main

---
+++ build/precache-manifest.2fe9b1312b37d04b5b49a9e3526f2380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f489de46fd44e18d62dfb15a59936cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183c7ff17761400355df",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d43fbe5e4de6e0ebf4a",
+    "url": "/static/js/2.86907938.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.86907938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183c7ff17761400355df",
+    "url": "/static/js/main.8b20e337.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
